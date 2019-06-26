37cb4599275a6675bbf231f2ea81e126d536cd28
briighter <jaylima0@gmail.com>
2019-06-25 22:42:33 -0400

    Add ability to click on RPS choice and reveal the value of the button

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+// get button objects from DOM
+let rock_btn = document.getElementById('rock');
+let paper_btn = document.getElementById('paper');
+let scissors_btn = document.getElementById('scissors');
+
+// Get score objects
+let playerScore = document.getElementById('player-score');
+let computerScore = document.getElementById('computer-score');
+
+// Initialize scores
+let playScore = 0;
+let cpuScore = 0;
+
+rock_btn.addEventListener("click", playGame);
+paper_btn.addEventListener("click", playGame);
+scissors_btn.addEventListener("click", playGame);
+
+
+function playGame() {
+    //let playerChoice = document.getElementById(e.id).value;
+    let compRoll = Math.random();
+    
+    alert(this.value);
+}
