aea3ee9cf60af6710e1e3f3ff5390cdef1e36ea8
briighter <jaylima0@gmail.com>
2019-08-04 13:46:02 -0400

    Change button value. Add game logic and score update

--- scripts/main.js
+++ scripts/main.js
@@ -22,4 +22,42 @@ function playGame() {
     let compChoice = compRoll.toString().substr(0,1);
     alert(this.value);
     alert(compChoice);
+
+    switch (Number(this.value)) {
+        case 0:
+            if (Number(compChoice) == 1) {
+                //computer wins
+                cpuScore++;
+                computerScore.innerHTML = cpuScore;
+            } else if (Number(compChoice) == 2) {
+                // player wins
+                playScore++;
+                playerScore.innerHTML = playScore;
+            }
+            break;
+        case 1:
+            if (Number(compChoice) == 2) {
+                //computer wins
+                cpuScore++;
+                computerScore.innerHTML = cpuScore;
+            } else if (Number(compChoice) == 0) {
+                // player wins
+                playScore++;
+                playerScore.innerHTML = playScore;
+            }
+            break;
+        case 2:
+            if (Number(compChoice) == 0) {
+                //computer wins
+                cpuScore++;
+                computerScore.innerHTML = cpuScore;
+            } else if (Number(compChoice) == 1) {
+                // player wins
+                playScore++;
+                playerScore.innerHTML = playScore;
+            }
+            break;
+        default:
+            break;
+    }
 }
